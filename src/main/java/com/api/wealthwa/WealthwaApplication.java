1bf29932ffcf08306527e7622a3af87b9fc16f91
AXE\e306299 <rohit.raj@wescodist.com>
2022-05-22 14:21:35 +0530

    security implements in swagger

--- src/main/java/com/api/wealthwa/WealthwaApplication.java
+++ src/main/java/com/api/wealthwa/WealthwaApplication.java
@@ -1,11 +1,15 @@
 package com.api.wealthwa;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "wealth-api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition(info = @Info(title = "Wealth API", version = "2.0", description = "Wealth Information"))
 public class WealthwaApplication {
     public static void main(String[] args) {
